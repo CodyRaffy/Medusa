4ef0243efd7e89fb2b1c1ae4795a7e4eef46c9f6
Cody Raffensperger <cody.raffensperger@gmail.com>
2018-05-15 22:19:35 -0400

    Add data, key and medusaTest model

---
+++ models/MedusaKey.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+namespace Medusa
+{
+    public class MedusaKey
+    {
+        public MedusaKey()
+        {
+            Answers = new List<string> {
+"B",
+"B",
+"A",
+"B",
+"B",
+"D",
+"D",
+"B",
+"A",
+"C",
+"D",
+"B",
+"D",
+"B",
+"C",
+"C",
+"C",
+"A",
+"B",
+"C",
+"B",
+"B",
+"All",
+"A",
+"D",
+"A",
+"D",
+"D",
+"D",
+"D",
+"D",
+"A",
+"C",
+"B",
+"B",
+"All",
+"A",
+"B",
+"D",
+"B",
+            };
+        }
+
+        public List<String> Answers { get; set; }
+    }
+}
---
+++ models/MedusaTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace Medusa
+{
+    public class MedusaTest
+    {
+        public string School { get; set; }
+
+        public string Subject { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public int GradeLevel { get; set; }
+
+        public List<String> Answers { get; set; }
+    }
+}
